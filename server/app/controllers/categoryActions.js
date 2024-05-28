5a5d3365a7879b424e3db38c23e31e9b0354b5aa
Your Name <you@example.com>
2024-05-28 13:51:05 +0200

    exo6 fini

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,32 +1,41 @@
+const tables = require("../../database/tables");
+
 // Some data to make the trick
 
-const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
+// const categories = [
+//     {
+//       id: 1,
+//       name: "Science-Fiction",
+//     },
+//     {
+//       id: 2,
+//       name: "Comédie",
+//     },
+//   ];
   
   // Declare the actions
   
   /* Here you code */
-  const browse = (req, res) => {
-    if (req.query.q != null) {
-      const filteredCategories = categories.filter((category) =>
-        category.name.includes(req.query.q)
-      );
-      res.json(filteredCategories);
-    } else {
-      res.json(categories);
-    }
+  // const browse = (req, res) => {
+  //   if (req.query.q != null) {
+  //     const filteredCategories = categories.filter((category) =>
+  //       category.name.includes(req.query.q)
+  //     );
+  //     res.json(filteredCategories);
+  //   } else {
+  //     res.json(categories);
+  //   }
+  // };
+
+  const browse = async (req,res) => {
+    const categoriesFromDB = await tables.category.readAll();
+    res.json(categoriesFromDB);
   };
   
   const read = (req, res) => {
     const parsedId = parseInt(req.params.id, 10);
+    
+    // eslint-disable-next-line no-undef
     const category = categories.find((c) => c.id === parsedId);
    
   
